284c6e69d8955deb464d57306dce0c35ffdf1a39
Pratheekjshetty <pratheekjshetty6696@gmail.com>
2024-06-06 12:46:58 +0530

    first-commit

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Link } from "react-router-dom"
-
+import "./App.css"
 export default function Home() {
   return (
         <div style={{color: "red",background:"linear-gradient(140deg,rgb(253, 78, 113),yellow)",height:"100vh"}}>
         <center><h1>Home</h1></center>
-        <nav style={{color: "black"}}>
+        <nav>
             <ol>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
